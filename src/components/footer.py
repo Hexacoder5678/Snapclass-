ee778f8d76dca080df96fa90628b96db1261efba
Archit Kumar Savita <skumar69688@gmail.com>
2026-06-07 13:27:31 +0530

    Fix database schema mismatches, import errors, and local QR code layout

--- src/components/footer.py
+++ src/components/footer.py
@@ -3,10 +3,20 @@ import streamlit as st
 
 def footer_home():
     
+    st.markdown(f"""
+        <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
+        <p style="font-weight:bold; color:white;"> Created with ❤️ by Archit Kumar Savita </p>  
+       
+        </div>
+                
+                """, unsafe_allow_html=True)
 
-    st.markdown("""
-       <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
-       <p style="font-weight:bold;color:white">Created  with love by Archit Kumar Savita</p>
-       </div>
 
-""",unsafe_allow_html=True)
+def footer_dashboard():
+    
+    st.markdown(f"""
+        <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
+        <p style="font-weight:bold; color:black;"> Created with ❤️ by Archit Kumar Savita</p>  
+        </div>
+                
+                """, unsafe_allow_html=True)
